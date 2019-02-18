aa00bf15304374b254864fde17380f6ced8d40e7
zifan feng <zifanfeng@gmail.com>
2019-02-18 10:22:34 -0600

    change ui

---
+++ source/scripts/data.js
@@ -0,0 +1,44 @@
+var exp_jsons = [
+  {
+    company: "Moon",
+    title: "Mooncake Maker",
+    location: "FAAAAAAAAaaaaaAAAAAR",
+    description:
+      "The Moon is humanity’s nearest companion in our travels in space and the only celestial body that we have had the chance to actually visit. Still, despite its relative closeness and familiarity, our satellite continues to hold many interesting secrets. From its scientific strangeness to the many ways it affects our lives, the Moon is a mystery that is definitely worth a closer look."
+  },
+  {
+    company: "Sleepy Beauty",
+    title: "Waker",
+    location: "Dorms",
+    description:
+      "Filled with jealousy, the evil witch Maleficent (Eleanor Audley) curses Princess Aurora (Mary Costa) to die on her 16th birthday. Thanks to Aurora's guardian fairies (Verna Felton, Barbara Jo Allen, Barbara Luddy), she only falls into a deep sleep that can be ended with a kiss from her betrothed, P"
+  }
+];
+
+var work_jsons = [
+  {
+    title: "Amazon Review Analyze",
+    area: "NLP",
+    detail: "but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, b"
+  },
+  {
+    title: "Auto-Translate App",
+    area: "Android development",
+    detail: " editors like MS Word or Lotus Notes generate random lorem text when needed, either as pre-installed module or plug-in to be added. Word selection or sequence don't necessarily match the original, which is intended to add variety. Presentation software like Keynote or Pages use it as a samples for screenplay layout. Content management software as Joomla, Drupal, Mambo"
+  },
+  {
+    title: "Data Processing App",
+    area: "R Shiny App",
+    detail: " and potentially serious design flaws. A seemingly elegant design can quickly begin to bloat with unexpected content or break under the weight of actual activity. Fake data can ensure a nice looking layout but it doesn’t reflect what a living, breathing a"
+  },
+  {
+    title: "UIUC Gender Visualization",
+    area: "D3.js",
+    detail: " professional use templating systems. Commercial publishing platforms and content management systems ensure that you can show different text, different data using the same template. When it's about controlling hundreds of articles, product pages for web shops, or user profiles in social networks, al"
+  },
+  {
+    title: "Space Invader",
+    area: "C++",
+    detail: "lient to pay no attention Lorem Ipsum isn't hard as it doesn’t make sense in the first place, that will limit any initial interest soon enough. Try telling a client to ignore draft copy however, and you're up to something you can't win. Whenever draft copy comes up in a meeting confused questions about it ensue. editors like MS Word or Lotus Notes generate random lorem text when needed, either as pre-installed module or plug-in to be added. Word selection or sequence don't necessarily match the original, which is intended to add variety. Presentation software like Keynote or Pages use it as a samples for screenplay layout. Content management software as Joomla, Drupal, Mambo"
+  }
+];
--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,3 +1,48 @@
+var exp_jsons = [
+  {
+    company: "Moon",
+    title: "Mooncake Maker",
+    location: "FAAAAAAAAaaaaaAAAAAR",
+    description:
+      "The Moon is humanity’s nearest companion in our travels in space and the only celestial body that we have had the chance to actually visit. Still, despite its relative closeness and familiarity, our satellite continues to hold many interesting secrets. From its scientific strangeness to the many ways it affects our lives, the Moon is a mystery that is definitely worth a closer look."
+  },
+  {
+    company: "Sleepy Beauty",
+    title: "Waker",
+    location: "Dorms",
+    description:
+      "Filled with jealousy, the evil witch Maleficent (Eleanor Audley) curses Princess Aurora (Mary Costa) to die on her 16th birthday. Thanks to Aurora's guardian fairies (Verna Felton, Barbara Jo Allen, Barbara Luddy), she only falls into a deep sleep that can be ended with a kiss from her betrothed, P"
+  }
+];
+
+var work_jsons = [
+  {
+    title: "Amazon Review Analyze",
+    area: "NLP",
+    detail: "but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, b"
+  },
+  {
+    title: "Auto-Translate App",
+    area: "Android development",
+    detail: " editors like MS Word or Lotus Notes generate random lorem text when needed, either as pre-installed module or plug-in to be added. Word selection or sequence don't necessarily match the original, which is intended to add variety. Presentation software like Keynote or Pages use it as a samples for screenplay layout. Content management software as Joomla, Drupal, Mambo"
+  },
+  {
+    title: "Data Processing App",
+    area: "R Shiny App",
+    detail: " and potentially serious design flaws. A seemingly elegant design can quickly begin to bloat with unexpected content or break under the weight of actual activity. Fake data can ensure a nice looking layout but it doesn’t reflect what a living, breathing a"
+  },
+  {
+    title: "UIUC Gender Visualization",
+    area: "D3.js",
+    detail: " professional use templating systems. Commercial publishing platforms and content management systems ensure that you can show different text, different data using the same template. When it's about controlling hundreds of articles, product pages for web shops, or user profiles in social networks, al"
+  },
+  {
+    title: "Space Invader",
+    area: "C++",
+    detail: "lient to pay no attention Lorem Ipsum isn't hard as it doesn’t make sense in the first place, that will limit any initial interest soon enough. Try telling a client to ignore draft copy however, and you're up to something you can't win. Whenever draft copy comes up in a meeting confused questions about it ensue. editors like MS Word or Lotus Notes generate random lorem text when needed, either as pre-installed module or plug-in to be added. Word selection or sequence don't necessarily match the original, which is intended to add variety. Presentation software like Keynote or Pages use it as a samples for screenplay layout. Content management software as Joomla, Drupal, Mambo"
+  }
+];
+
 var intro_div = document.querySelector(".intro-div");
 var experience_div = document.querySelector(".experience-div");
 var works_div = document.querySelector(".works-div");
@@ -74,50 +119,7 @@ function add_random_carousal(random_div) {
   }
 }
 
-var exp_jsons = [
-  {
-    company: "Moon",
-    title: "Mooncake Maker",
-    location: "FAAAAAAAAaaaaaAAAAAR",
-    description:
-      "The Moon is humanity’s nearest companion in our travels in space and the only celestial body that we have had the chance to actually visit. Still, despite its relative closeness and familiarity, our satellite continues to hold many interesting secrets. From its scientific strangeness to the many ways it affects our lives, the Moon is a mystery that is definitely worth a closer look."
-  },
-  {
-    company: "Sleepy Beauty",
-    title: "Waker",
-    location: "Dorms",
-    description:
-      "Filled with jealousy, the evil witch Maleficent (Eleanor Audley) curses Princess Aurora (Mary Costa) to die on her 16th birthday. Thanks to Aurora's guardian fairies (Verna Felton, Barbara Jo Allen, Barbara Luddy), she only falls into a deep sleep that can be ended with a kiss from her betrothed, P"
-  }
-];
 
-var work_jsons = [
-  {
-    title: "Amazon Review Analyze",
-    area: "NLP",
-    detail: "but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, b"
-  },
-  {
-    title: "Auto-Translate App",
-    area: "Android development",
-    detail: " editors like MS Word or Lotus Notes generate random lorem text when needed, either as pre-installed module or plug-in to be added. Word selection or sequence don't necessarily match the original, which is intended to add variety. Presentation software like Keynote or Pages use it as a samples for screenplay layout. Content management software as Joomla, Drupal, Mambo"
-  },
-  {
-    title: "Data Processing App",
-    area: "R Shiny App",
-    detail: " and potentially serious design flaws. A seemingly elegant design can quickly begin to bloat with unexpected content or break under the weight of actual activity. Fake data can ensure a nice looking layout but it doesn’t reflect what a living, breathing a"
-  },
-  {
-    title: "UIUC Gender Visualization",
-    area: "D3.js",
-    detail: " professional use templating systems. Commercial publishing platforms and content management systems ensure that you can show different text, different data using the same template. When it's about controlling hundreds of articles, product pages for web shops, or user profiles in social networks, al"
-  },
-  {
-    title: "Space Invader",
-    area: "C++",
-    detail: "lient to pay no attention Lorem Ipsum isn't hard as it doesn’t make sense in the first place, that will limit any initial interest soon enough. Try telling a client to ignore draft copy however, and you're up to something you can't win. Whenever draft copy comes up in a meeting confused questions about it ensue. editors like MS Word or Lotus Notes generate random lorem text when needed, either as pre-installed module or plug-in to be added. Word selection or sequence don't necessarily match the original, which is intended to add variety. Presentation software like Keynote or Pages use it as a samples for screenplay layout. Content management software as Joomla, Drupal, Mambo"
-  }
-];
 add_experience_block(exp_jsons, experience_div);
 add_work_block(work_jsons, works_div);
 
@@ -163,10 +165,9 @@ var contactY = document.querySelector(".end-div").offsetTop + site_wrapY;
 
 function toggleFix() {
   
-  let scrollY = this.scrollY;
+  let scrollY = this.scrollY + 1;
   let nav_curHeight = nav.getBoundingClientRect().height;
   if (scrollY >= (introY)) {
-    console.log(nav_curHeight, scrollY, navY);
     nav.style.position="fixed";
     document.body.classList.add("nav-fix");
     // site_wrap.style.paddingTop = nav_intro.clientHeight + "px";
@@ -199,7 +200,6 @@ function toggleFix() {
   }
 
   if (scrollY >= randomY&& scrollY < contactY ) {
-    console.log("in random", scrollY, randomY, workY);
     nav_random.classList.add("nav-highlight");
   } else {
     nav_random.classList.remove("nav-highlight");
@@ -214,9 +214,11 @@ function toggleFix() {
 }
 window.addEventListener("scroll", toggleFix);
 nav_contact.addEventListener("click", function(e) {
+  console.log(contactY, document.querySelector(".end-div").offsetTop + site_wrapY);
   window.scrollTo({ top: contactY, behavior: "smooth" });
 });
 nav_exp.addEventListener("click", function(e) {
+  console.log("exp", experienceY, experience_div.offsetTop + site_wrapY);
   window.scrollTo({ top: experienceY, behavior: "smooth" });
 });
 nav_works.addEventListener("click", function(e) {
@@ -235,8 +237,7 @@ logo.addEventListener("click", function(e) {
 
 var prev_slide = document.querySelector(".prev");
 var next_slide = document.querySelector(".next");
-// prev_slide.addEventListener("click", plusSlides(-1), false);
-// next_slide.addEventListener("click", plusSlides(1), false);
+
 
 var works_items = document.querySelectorAll(".works-item");
 var works_modal = document.querySelector(".works-modal");
@@ -257,3 +258,4 @@ works_items.forEach(function(item, idx) {
     `;
   }, false);
 })
+
